704519213ece6824a66f591161ae8ac80482386a
Ricard Lado <ricardlado@hotmail.com>
2023-10-02 10:00:09 +0200

    Initial commit: v0.1.0

---
+++ README.md
@@ -0,0 +1,6 @@
+# Oink! packaging script for debian based systems
+
+> Requirements *dpkg-deb*, *git*, *go*, *make*
+```bash
+bash build_deb.sh
+```
---
+++ makepkg_deb.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+VERSION="0.1"
+REV="0"
+ARCH=uname -m
+
+# This script is used to build a Debian package from source
+git clone https://github.com/RLado/Oink.git
+git checkout v$VERSION.$REV
+
+# Build
+cd Oink
+make build
+
+cd ..
+
+# Create the control file
+echo "
+Package: oink
+Version: $VERSION.$REV
+Architecture: $ARCH
+Maintainer: Ricard Lado <ricard@lado.one>
+Description: A lightweight DDNS client for Porkbun
+    Oink! is an unofficial DDNS client for porkbun.com built in Go
+" > control
+
+# Create a temporary directory
+mkdir -p oink_$VERSION-$REV_$ARCH
+mkdir -p oink_$VERSION-$REV_$ARCH/DEBIAN
+mkdir -p oink_$VERSION-$REV_$ARCH/usr/bin
+mkdir -p oink_$VERSION-$REV_$ARCH/etc/oink_ddns/
+mkdir -p oink_$VERSION-$REV_$ARCH/lib/systemd/system/
+
+# Copy files
+cp Oink/oink oink_$VERSION-$REV_$ARCH/usr/bin/
+cp Oink/config/config.json oink_$VERSION-$REV_$ARCH/etc/oink_ddns/
+cp Oink/config/oink_ddns.service oink_$VERSION-$REV_$ARCH/lib/systemd/system/
+cp control  oink_$VERSION-$REV_$ARCH/DEBIAN/
+
+# Create the package
+dpkg-deb --build --root-owner-group oink_$VERSION-$REV_$ARCH
+
+# Clean up
+rm -rf oink_$VERSION-$REV_$ARCH
+rm -rf Oink
+rm control
